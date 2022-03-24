dd4112e7b006cd970c499b5ae5ca7bbf420491c2
Jessie <miss_sc_jessi@hotmail.com>
2022-03-24 12:09:02 -0300

    create screen home

---
+++ klever-io/src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+import TableToken from '../../components/table/TableToken';
+import './Home.css'
+
+function Home() {
+  return (
+    <>
+      <Header home/>
+      <TableToken />
+    </>
+  );
+}
+
+export default Home;
