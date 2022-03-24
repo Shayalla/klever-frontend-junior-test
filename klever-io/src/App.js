169993bdcdb1c5335114f0e2289e3167521ae07b
Jessie <miss_sc_jessi@hotmail.com>
2022-03-24 15:24:36 -0300

    create route add token

--- klever-io/src/App.js
+++ klever-io/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import AddToken from './pages/addToken/AddToken';
 import Home from './pages/home/Home';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Home /> } />
-      <Route path="/add-token" element={ <Home /> } />
+      <Route path="/add-token" element={ <AddToken /> } />
       <Route path="/edit-token" element={ <Home /> } />
     </Routes>
   );
