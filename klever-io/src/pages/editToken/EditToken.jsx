d3221809c15c2212017952243da9940e704d70b6
Jessie <miss_sc_jessi@hotmail.com>
2022-03-24 18:28:33 -0300

    create screen edit Token

---
+++ klever-io/src/pages/editToken/EditToken.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useParams } from 'react-router';
+import FormEditToken from '../../components/formEditToken/FormEditToken';
+import Header from '../../components/header/Header';
+import './EditToken.css'
+
+function EditToken() {
+  const { token } = useParams();
+
+  return (
+    <>
+      <Header />
+      <FormEditToken token={ token } />
+    </>
+  );
+}
+
+export default EditToken;
