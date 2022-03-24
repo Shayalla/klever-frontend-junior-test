3286af18b1f33ea89f039146fe4969d408787140
Jessie <miss_sc_jessi@hotmail.com>
2022-03-24 15:23:57 -0300

    create screen add token

---
+++ klever-io/src/pages/addToken/AddToken.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import FormAddToken from '../../components/formAddToken/FormAddToken';
+import Header from '../../components/header/Header';
+import './AddToken.css'
+
+function AddToken() {
+  return (
+    <>
+      <Header />
+      <FormAddToken />
+    </>
+  );
+}
+
+export default AddToken;
